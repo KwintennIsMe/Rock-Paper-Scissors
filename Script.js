75f2061289e00f6a96ddaa6d81cddd44f80e958b
KwintennIsMe <122505466+KwintennIsMe@users.noreply.github.com>
2023-02-08 19:31:11 -0800

    Add lines for player losing

--- Script.js
+++ Script.js
@@ -10,12 +10,13 @@ function getRandomInt() {
 function getComputerChoice() {
     let choiceNum = getRandomInt()
     if (choiceNum === 0) {
-        computerChoice = "Rock";
+        computerChoice = "rock";
     } else if (choiceNum === 1) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     } else if (choiceNum === 2) {
-        computerChoice = "Scissors"
+        computerChoice = "scissors"
     }
+    return computerChoice;
 }
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
@@ -24,15 +25,32 @@ function playRound(playerSelection, computerSelection) {
         gameDeclaration = "You win! Paper beats Rock!";
         return gameDeclaration;
         winStatus = ++winStatus;
-    } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
         gameDeclaration = "You win! Rock beats Scissors!";
         return gameDeclaration;
         winStatus = ++winStatus;
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
         gameDeclaration = "You win! Scissors beats Paper!";
         return gameDeclaration;
         winStatus = ++winStatus;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        gameDeclaration = "You lose! Scissors beats Paper!"
+        return gameDeclaration;
+        loseStatus = ++loseStatus;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        gameDeclaration = "You lose! Rock beats Scissors!"
+        return gameDeclaration;
+        loseStatus = ++loseStatus;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        gameDeclaration = "You lose! Paper beats Rock!";
+        return gameDeclaration;
+        loseStatus = ++loseStatus;
+    } else if ()
+      else {
+        console.log("Something went wrong.")
     }
+    console.log(gameDeclaration);
 }
-getComputerChoice();
-console.log(computerChoice);
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
