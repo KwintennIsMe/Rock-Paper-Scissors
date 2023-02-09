1b832f6b614f03cdb412539b14e936a7fd83bb2b
KwintennIsMe <122505466+KwintennIsMe@users.noreply.github.com>
2023-02-08 19:20:03 -0800

    Add winning declarations for player

--- Script.js
+++ Script.js
@@ -1,6 +1,8 @@
 console.log("Hello world")
-let computerChoice
-let playerChoice
+let computerChoice;
+let playerChoice;
+let winStatus;
+let loseStatus;
 
 function getRandomInt() {
     return Math.floor(Math.random() * 3);
@@ -13,6 +15,23 @@ function getComputerChoice() {
         computerChoice = "Paper";
     } else if (choiceNum === 2) {
         computerChoice = "Scissors"
+    }
+}
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let gameDeclaration;
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        gameDeclaration = "You win! Paper beats Rock!";
+        return gameDeclaration;
+        winStatus = ++winStatus;
+    } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+        gameDeclaration = "You win! Rock beats Scissors!";
+        return gameDeclaration;
+        winStatus = ++winStatus;
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        gameDeclaration = "You win! Scissors beats Paper!";
+        return gameDeclaration;
+        winStatus = ++winStatus;
     }
 }
 getComputerChoice();
